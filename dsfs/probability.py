ccfa5e73aa9671666d6c211ee3103a1cd8e0a660
laj <larry.jones@reveal-energy.com>
2022-12-28 03:24:43 -0600

    Normal PDF

--- dsfs/probability.py
+++ dsfs/probability.py
@@ -1,9 +1,12 @@
-def uniform_pdf(x):
+import math
+
+
+def uniform_pdf(x: float) -> float:
     """Calculate the uniform probability density function at `x`"""
     return x if 0 <= x < 1 else 0
 
 
-def uniform_cdf(x):
+def uniform_cdf(x: float) -> float:
     """Calculate the amount of probability in the uniform PDF less than or equal to `x`."""
     match x:
         # No probability is below 0
@@ -15,3 +18,11 @@ def uniform_cdf(x):
         # All the probability is contained in region whose values are less than or equal 1
         case _:
             return 1
+
+
+SQRT_TWO_PI = math.sqrt(2 * math.pi)
+
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    """Calculate the normal probability density at `x`"""
+    return (1 / (SQRT_TWO_PI * sigma)) * math.exp(- ((x - mu) ** 2) / (2 * sigma ** 2))
--- test/probability_test.py
+++ test/probability_test.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 import dsfs as scratch
@@ -29,3 +31,16 @@ def test_uniform_pdf(x, expected):
 )
 def test_uniform_cdf(x, expected):
     assert scratch.probability.uniform_cdf(x) == expected
+
+
+@pytest.mark.parametrize(
+    'mu, sigma, x, expected',
+    [
+        (0, 1, 0, 1 / ((math.sqrt(2 * math.pi)) * 1)),
+        (0, 2, 0, 1 / ((math.sqrt(2 * math.pi)) * 2)),
+        (0, 0.5, 0, 1 / ((math.sqrt(2 * math.pi)) * 0.5)),
+        (-1, 1, -1, 1 / ((math.sqrt(2 * math.pi)) * 1)),
+    ]
+)
+def test_normal_pdf(mu, sigma, x, expected):
+    assert scratch.probability.normal_pdf(x, mu=mu, sigma=sigma) == pytest.approx(expected)
