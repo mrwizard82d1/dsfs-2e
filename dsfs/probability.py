8d6085021c1cf29446bdbccf20a50430c63b8b69
laj <larry.jones@reveal-energy.com>
2022-12-29 19:30:32 -0600

    Normal CDF

--- dsfs/probability.py
+++ dsfs/probability.py
@@ -26,3 +26,8 @@ SQRT_TWO_PI = math.sqrt(2 * math.pi)
 def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
     """Calculate the normal probability density at `x`"""
     return (1 / (SQRT_TWO_PI * sigma)) * math.exp(- ((x - mu) ** 2) / (2 * sigma ** 2))
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    """Calculate the probability that a random variable distributed as N(`mu`, `sigma`) is less than or equal to `x`"""
+    return (1 + math.erf((x - mu) / (math.sqrt(2) * sigma))) / 2
--- test/probability_test.py
+++ test/probability_test.py
@@ -44,3 +44,22 @@ def test_uniform_cdf(x, expected):
 )
 def test_normal_pdf(mu, sigma, x, expected):
     assert scratch.probability.normal_pdf(x, mu=mu, sigma=sigma) == pytest.approx(expected)
+
+
+@pytest.mark.parametrize(
+    'mu, sigma, p, expected',
+    [
+        (0, 1, -3, 0.001350),
+        (0, 1, -2, 0.022750),
+        (0, 1, -1, 0.158655),
+        (0, 1, 0, 0.5),
+        (0, 1, 1, 0.841345),
+        (0, 1, 2, 0.977250),
+        (0, 1, 3, 0.998650),
+        (0, 2, 4.154418, 0.981109),
+        (0, 0.5, -1.694350, 0.000351),
+        (-1, 1, 1.390205, 0.991580),
+    ]
+)
+def test_normal_cdf(mu, sigma, p, expected):
+    assert scratch.probability.normal_cdf(p, mu=mu, sigma=sigma) == pytest.approx(expected, abs=1e-6)
